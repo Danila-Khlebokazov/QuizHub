0d77556059d81877552a3d1ca8e9952f17b9f071
Danila Khlebokazov <danilik_2003@mail.ru>
2023-04-04 22:10:30 +0600

    routing added

--- quizhub/src/app/app-routing.module.ts
+++ quizhub/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainPageComponent} from "./main-page/main-page.component";
+import {UserPageComponent} from "./user-page/user-page.component";
+import {QuizPageComponent} from "./quiz-page/quiz-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "main", component: MainPageComponent},
+  {path: ":userId", component: UserPageComponent},
+  {path: ":userId/favorites", component: UserPageComponent},
+  {path: ":userId/quizzes", component: UserPageComponent},
+  {path: ":userId/:quizId", component: QuizPageComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
